bce0f037a3339daa660c1f3477158cb47e98ecfc
anshariF17@gmail.com <anshariF17@gmail.com>
2023-07-12 16:34:33 +0700

    progressbar update

--- Assets/Scripts/AchievetmentSession.cs
+++ Assets/Scripts/AchievetmentSession.cs
@@ -34,15 +34,15 @@ public class AchievetmentSession : MonoBehaviour
     }
     private void Start()
     {
-        progressBarValue = progressBar.value;
-        progressBar.maxValue = 3;
-        progressBar.value = 0;
+        // progressBarValue = progressBar.value;
+        // progressBar.maxValue = 3;
+        // progressBar.value = 0;
         int currentScene = SceneManager.GetActiveScene().buildIndex;
-        if (currentScene == 0)
-        {
-            starCanvas.gameObject.SetActive(false);
-            keyPad.gameObject.SetActive(false);
-        }
+        // if (currentScene == 0)
+        // {
+        //     starCanvas.gameObject.SetActive(false);
+        //     keyPad.gameObject.SetActive(false);
+        // }
 
 
     }
@@ -50,73 +50,73 @@ public class AchievetmentSession : MonoBehaviour
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
 
-        switch (currentScene)
-        {
-            case 0:
-                starCanvas.gameObject.SetActive(false);
-                keyPad.gameObject.SetActive(false);
-                progressBar.value = 0;
-                break;
-            case 3:
-                starCanvas.gameObject.SetActive(false);
-                keyPad.gameObject.SetActive(false);
-                progressBar.value = 0;
-                break;
-            case 4:
-                starCanvas.gameObject.SetActive(true);
-                keyPad.gameObject.SetActive(true);
-                progressBar.value = 1;
-                break;
-            case 5:
-                progressBar.value = 2;
-                break;
-            case 6:
-                progressBar.value = 3;
-                break;
-            case 7:
-                starCanvas.gameObject.SetActive(true);
-                keyPad.gameObject.SetActive(true);
-                progressBar.value = 1;
-                progressBar.maxValue = 11;
-                break;
-            case 8:
-                progressBar.value = 2;
-                break;
-            case 9:
-                progressBar.value = 3;
-                break;
-            case 10:
-                progressBar.value = 4;
-                break;
-            case 11:
-                progressBar.value = 5;
-                break;
-            case 12:
-                progressBar.value = 6;
-                break;
-            case 13:
-                progressBar.value = 7;
-                break;
-            case 14:
-                progressBar.value = 8;
-                break;
-            case 15:
-                progressBar.value = 9;
-                break;
-            case 16:
-                progressBar.value = 10;
-                break;
-            case 17:
-                progressBar.value = 11;
-                break;
-            case 18:
-                starCanvas.gameObject.SetActive(true);
-                keyPad.gameObject.SetActive(true);
-                progressBar.value = 1;
-                progressBar.maxValue = 1;
+        // switch (currentScene)
+        // {
+        //     case 0:
 
-                break;
-        }
+        //         keyPad.gameObject.SetActive(false);
+        //         progressBar.value = 0;
+        //         break;
+        //     case 3:
+
+        //         keyPad.gameObject.SetActive(false);
+        //         progressBar.value = 0;
+        //         break;
+        //     case 4:
+
+
+        //         progressBar.value = 1;
+        //         break;
+        //     case 5:
+        //         progressBar.value = 2;
+        //         break;
+        //     case 6:
+        //         progressBar.value = 3;
+        //         break;
+        //     case 7:
+
+        //         keyPad.gameObject.SetActive(true);
+        //         progressBar.value = 1;
+        //         progressBar.maxValue = 11;
+        //         break;
+        //     case 8:
+        //         progressBar.value = 2;
+        //         break;
+        //     case 9:
+        //         progressBar.value = 3;
+        //         break;
+        //     case 10:
+        //         progressBar.value = 4;
+        //         break;
+        //     case 11:
+        //         progressBar.value = 5;
+        //         break;
+        //     case 12:
+        //         progressBar.value = 6;
+        //         break;
+        //     case 13:
+        //         progressBar.value = 7;
+        //         break;
+        //     case 14:
+        //         progressBar.value = 8;
+        //         break;
+        //     case 15:
+        //         progressBar.value = 9;
+        //         break;
+        //     case 16:
+        //         progressBar.value = 10;
+        //         break;
+        //     case 17:
+        //         progressBar.value = 11;
+        //         break;
+        //     case 18:
+
+        //         keyPad.gameObject.SetActive(true);
+        //         progressBar.value = 1;
+        //         progressBar.maxValue = 1;
+
+        //         break;
+        // }
 
         // if (currentScene == 0)
         // {
@@ -193,7 +193,7 @@ public class AchievetmentSession : MonoBehaviour
 
             if (nextSceneIndex == 18 && !udahTamat)
             {
-
+                PlayerPrefs.SetInt("levelAt", 4);
                 misi2.interactable = true;
                 misi3.interactable = true;
 
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
     public int target = 2;
     [SerializeField] GameObject exit;
+    [SerializeField] Slider progressBar;
+
 
 
     void Start()
@@ -21,12 +25,66 @@ public class GameSession : MonoBehaviour
     }
     public void ProcessNextLevel()
     {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
         if (target == 0)
         {
             exit.SetActive(true);
             DestroyNonTarget();
             DestroyCanvas();
-
+            switch (currentScene)
+            {
+                case 0:
+                    progressBar.value = 0;
+                    break;
+                case 3:
+                    progressBar.value = 0;
+                    break;
+                case 4:
+                    progressBar.value = 1;
+                    break;
+                case 5:
+                    progressBar.value = 2;
+                    break;
+                case 6:
+                    progressBar.value = 3;
+                    break;
+                case 7:
+                    progressBar.value = 1;
+                    break;
+                case 8:
+                    progressBar.value = 2;
+                    break;
+                case 9:
+                    progressBar.value = 3;
+                    break;
+                case 10:
+                    progressBar.value = 4;
+                    break;
+                case 11:
+                    progressBar.value = 5;
+                    break;
+                case 12:
+                    progressBar.value = 6;
+                    break;
+                case 13:
+                    progressBar.value = 7;
+                    break;
+                case 14:
+                    progressBar.value = 8;
+                    break;
+                case 15:
+                    progressBar.value = 9;
+                    break;
+                case 16:
+                    progressBar.value = 10;
+                    break;
+                case 17:
+                    progressBar.value = 11;
+                    break;
+                case 18:
+                    progressBar.value = 1;
+                    break;
+            }
             // target = 4;
         }
     }
